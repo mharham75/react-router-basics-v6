6c785755aa85ce6ebd232ce36e2a7e4280c48860
Md Arham <mhrham75@gmaul.com>
2022-07-31 02:26:50 +0530

    added no route matched

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route} from 'react-router-dom';
 //components
 import Home from './components/Home';
 import About from './components/About';
+import Navbar from './components/Navbar';
+import OrderSummary from './components/Order-Summary';
+import NoMatch from './components/NoMatch'
 
 //styles
 import './App.css';
-import Navbar from './components/Navbar';
-import OrderSummary from './components/Order-Summary';
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='order-summary' element={<OrderSummary />} />
+        <Route path='*' element={<NoMatch />} />
       </Routes>  
     </div>
   );
