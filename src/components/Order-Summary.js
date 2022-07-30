d6a98a41a1daa741e4973d2acede4d2c9aba4704
Md Arham <mhrham75@gmaul.com>
2022-07-31 02:20:25 +0530

    Added back button using useNavigate
    :wq
     :wq

--- src/components/Order-Summary.js
+++ src/components/Order-Summary.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const OrderSummary = () => {
+
+    const navigate = useNavigate()
+
   return (
     <>
         <p>Your Order has been placed !</p>
+        <button onClick={ () => navigate(-1)}>Go Back</button>
     </>
   )
 }
